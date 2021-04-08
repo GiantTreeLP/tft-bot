9476d4e400bcfaa0ae76db7ef4f45416d6b516d1
Marvin <marv.1997@live.de>
2021-04-08 19:32:04 +0200

    Make proper use of classes Drop python-imagesearch in favor of win32 screenshot functionality Update requirements Fix lockfile path

--- tft.py
+++ tft.py
@@ -2,48 +2,58 @@
 # Branch: main
 from typing import Union
 
+import cv2
 import numpy as np
 import pkg_resources
 import requests
 import base64
 import os
 import pyautogui as auto
-from python_imagesearch.imagesearch import imagesearch as search
+from pygetwindow import Win32Window
 import time
 from printy import printy
 import urllib3
 import pydirectinput
+import win32con
+import win32gui
+import win32ui
 
-pkg_resources.require("PyAutoGUI==0.9.50")
-pkg_resources.require("opencv-python==4.2.0.34")
-pkg_resources.require("python-imageseach-drov0==1.0.6")
+pkg_resources.require("PyAutoGUI~=0.9.50")
+pkg_resources.require("opencv-python~=4.5.1.48")
 
 
 urllib3.disable_warnings()
 # If you haven't installed your game in default path (Windows) set your path here
-PATH = "default"
+PATH = r"G:\Games\Riot Games\League of Legends\lockfile"
 auto.FAILSAFE = False
 pydirectinput.FAILSAFE = False
 
-class xtra:
-    def base64encode(text):
+
+class Extra:
+
+    @staticmethod
+    def base64encode(text: str):
         text = base64.b64encode(text.encode("ascii")).decode("ascii")
         return text
 
-class lcu:
+
+class LCU:
+    @staticmethod
     def connect(lockfile_path="default"):
         if lockfile_path == "default":
             lockfile_path = "C:\\Riot Games\\League of Legends\\lockfile"
-        if lcu.check_exist(lockfile_path):
-            return lcu.readFile(lockfile_path)
+        if LCU.check_exist(lockfile_path):
+            return LCU.read_file(lockfile_path)
         else:
             raise Exception("Couldn't read lockfile!\nThis could mean that either the \
 path is not the right or the League Client is not opened!")
 
+    @staticmethod
     def check_exist(lockfile_path):
         return os.path.exists(lockfile_path)
 
-    def readFile(lockfile_path):
+    @staticmethod
+    def read_file(lockfile_path):
         lockfile = open(lockfile_path, "r")
         data = lockfile.read().split(":")
         data_dict = {
@@ -54,61 +64,57 @@ path is not the right or the League Client is not opened!")
         }
         return data_dict
 
+    @staticmethod
     def start_search(lcu_data):
-        auth = xtra.base64encode(lcu_data["auth"])
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": F"Basic {auth}"
-        }
+        headers = LCU.get_lcu_headers(lcu_data)
         url = lcu_data["url"] + "/lol-lobby/v2/lobby/matchmaking/search"
         request = requests.post(url, headers=headers, verify=False)
         return request.status_code
 
+    @staticmethod
     def play_again(lcu_data):
-        auth = xtra.base64encode(lcu_data["auth"])
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": F"Basic {auth}"
-        }
+        headers = LCU.get_lcu_headers(lcu_data)
         url = lcu_data["url"] + "/lol-lobby/v2/play-again"
         request = requests.post(url, headers=headers, verify=False)
         return request.status_code
 
-    def create_game_lobby_tft(lcu_data, gm:str="Normal"):
-        if gm.upper() != "NORMAL" and gm.upper() != "RANKED":
-            raise ValueError("Defined gamemode is invalid!\nValid gamemodes are Normal and Ranked.")
+    @staticmethod
+    def create_game_lobby_tft(lcu_data, gm: str = "Normal"):
+        if gm.upper() not in ("NORMAL", "RANKED"):
+            raise ValueError(
+                "Defined gamemode is invalid!\nValid gamemodes are Normal and Ranked.")
         else:
             if gm.upper() == "NORMAL":
                 q_id = 1090
             if gm.upper() == "RANKED":
                 q_id = 1100
-        auth = xtra.base64encode(lcu_data["auth"])
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": F"Basic {auth}"
-        }
+        headers = LCU.get_lcu_headers(lcu_data)
         data = '{{"queueId":{}}}'.format(q_id)
         url = lcu_data["url"] + "/lol-lobby/v2/lobby/"
         request = requests.post(url, headers=headers, data=data, verify=False)
         return request.status_code
 
+    @staticmethod
     def create_tutorial_lobby_tft(lcu_data):
-        auth = xtra.base64encode(lcu_data["auth"])
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": F"Basic {auth}"
-        }
+        headers = LCU.get_lcu_headers(lcu_data)
         data = '{"queueId":1110}'
         url = lcu_data["url"] + "/lol-lobby/v2/lobby/"
         request = requests.post(url, headers=headers, data=data, verify=False)
         return request.json()
 
+    @staticmethod
+    def get_lcu_headers(lcu_data):
+        auth = Extra.base64encode(lcu_data["auth"])
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        return headers
+
+    @staticmethod
     def get_current_summoner_in_queue(lcu_data, rc=False):
-        auth = xtra.base64encode(lcu_data["auth"])
+        auth: str = Extra.base64encode(lcu_data["auth"])
         headers = {
             "Accept": "application/json",
             "Authorization": F"Basic {auth}"
@@ -120,29 +126,26 @@ path is not the right or the League Client is not opened!")
             if request_json["state"] == "Invalid":
                 return True
             elif request_json["state"] == "InProgress":
-                if rc == True:
+                if rc:
                     return 2
                 return True
         elif request_json["httpStatus"] == 404:
             return False
 
+    @staticmethod
     def get_current_summoner_ready_check(lcu_data, rc=False):
-        is_in_q = lcu.get_current_summoner_in_queue(lcu_data,True)
+        is_in_q = LCU.get_current_summoner_in_queue(lcu_data, True)
         if is_in_q == 2:
             return True
         if is_in_q == True and is_in_q != 2 and rc == True:
             return 2
         return False
 
+    @staticmethod
     def auto_accept_current_ready_check(lcu_data):
-        ready_check = lcu.get_current_summoner_ready_check(lcu_data,True)
-        if ready_check == True:
-            auth = xtra.base64encode(lcu_data["auth"])
-            headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": F"Basic {auth}"
-            }
+        ready_check = LCU.get_current_summoner_ready_check(lcu_data, True)
+        if ready_check:
+            headers = LCU.get_lcu_headers(lcu_data)
             url = lcu_data["url"] + "/lol-matchmaking/v1/ready-check/accept"
             requests.post(url, headers=headers, verify=False)
             return "Accepted!"
@@ -151,44 +154,115 @@ path is not the right or the League Client is not opened!")
         else:
             return "Not in queue!"
 
+
 # Start utility methods
-class wrappers:
+class Wrappers:
 
+    IMAGE_CACHE = dict()
+
+    @staticmethod
     def search(image: Union[str, np.ndarray], precision: float = 0.8):
-        return search(image, precision)
+        # Optimization
+        if isinstance(image, str):
+            if not (image in Wrappers.IMAGE_CACHE):
+                im = cv2.imread(image, 0)
+                if im is None:
+                    raise
+                Wrappers.IMAGE_CACHE[image] = im
+            return Wrappers.search(Wrappers.IMAGE_CACHE[image], precision)
+        elif isinstance(image, np.ndarray):
+            window = Wrappers.get_tft_window()
+            im = Wrappers.screenshot(window)
+            # im = auto.screenshot(region=wrappers.get_tft_window().box)
+            # if is_retina:
+            #     im.thumbnail(
+            #         (round(im.size[0] * 0.5), round(im.size[1] * 0.5)))
+            # im = cv2.cvtColor(np.array(im), cv2.COLOR_BGR2GRAY)
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            res = cv2.matchTemplate(im, image, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            if max_val < precision:
+                return [-1, -1]
+            cv2.circle(im, max_loc, 16, (0, 0, 255, 255), 4)
+            cv2.namedWindow("test")
+            cv2.imshow("test", im)
+            cv2.waitKey(1)
+            return np.add(max_loc, window.topleft)
 
+    @staticmethod
+    def get_tft_window() -> Win32Window:
+        return auto.getWindowsWithTitle("League of Legends")[0]
+
+    @staticmethod
+    def screenshot(window: Win32Window) -> np.ndarray:
+        left, top, width, height = window.box
+        hDC = win32gui.GetWindowDC(window._hWnd)
+
+        myDC = win32ui.CreateDCFromHandle(hDC)
+        newDC = myDC.CreateCompatibleDC()
+
+        myBitMap = win32ui.CreateBitmap()
+        myBitMap.CreateCompatibleBitmap(myDC, width, height)
+
+        newDC.SelectObject(myBitMap)
+
+        newDC.BitBlt((0, 0), (width, height), myDC, (0, 0), win32con.SRCCOPY)
+        myBitMap.Paint(newDC)
+
+        array = np.fromstring(myBitMap.GetBitmapBits(True), dtype='uint8')
+        array.shape = (height, width, -1)
+
+        win32gui.DeleteObject(myBitMap.GetHandle())
+        newDC.DeleteDC()
+        myDC.DeleteDC()
+        win32gui.ReleaseDC(hDC, window._hWnd)
+
+        return array
+
+    @staticmethod
     def onscreen(path, precision=0.8):
-        return search(path, precision)[0] != -1
+        return Wrappers.search(path, precision)[0] != -1
 
+    @staticmethod
     def search_to(path):
-        pos = wrappers.search(path)
+        pos = Wrappers.search(path)
         # if wrappers.onscreen(path):
         if pos[0] != -1:
             auto.moveTo(pos)
             return pos
 
+    @staticmethod
     def click_key(key, delay=.1):
+        Wrappers.get_tft_window().activate()
         auto.press(key)
 
+    @staticmethod
     def click_left(delay=.1):
+        Wrappers.get_tft_window().activate()
         auto.leftClick()
 
+    @staticmethod
     def click_right(delay=.1):
+        Wrappers.get_tft_window().activate()
         auto.rightClick()
 
+    @staticmethod
     def click_to(path, delay=.1):
-        pos = wrappers.search(path)
+        pos = Wrappers.search(path)
         # if wrappers.onscreen(path):
         if pos[0] != -1:
             # auto.moveTo(pos)
+            Wrappers.get_tft_window().activate()
             auto.leftClick(pos)
             # wrappers.click_left(delay)
 
+    @staticmethod
     def click_to_r(path, delay=.1):
-        pos = wrappers.search(path)
+        pos = Wrappers.search(path)
         # if wrappers.onscreen(path):
         if pos[0] != -1:
             # auto.moveTo(pos)
+            Wrappers.get_tft_window().activate()
             auto.rightClick(pos)
             # wrappers.click_right(delay)
         # print(path + " clicked")
@@ -197,47 +271,48 @@ class wrappers:
 
 
 # Start main process
-class main:
-    def queue():
-        lcu_data = lcu.connect(PATH)
+class Main:
+
+    def __init__(self):
+        self.combo = 1
+
+    def queue(self):
+        lcu_data = LCU.connect(PATH)
         print("Creating tft lobby...")
-        lcu.create_game_lobby_tft(lcu_data)
-        lcu.start_search(lcu_data)
+        LCU.create_game_lobby_tft(lcu_data)
+        LCU.start_search(lcu_data)
         while True:
-            response = lcu.auto_accept_current_ready_check(lcu_data)
+            response = LCU.auto_accept_current_ready_check(lcu_data)
             if response == "Accepted!":
                 time.sleep(5)
-                response = lcu.auto_accept_current_ready_check(lcu_data)
+                response = LCU.auto_accept_current_ready_check(lcu_data)
                 if response == "Not in queue!":
                     break
             elif response == "Not in queue!":
                 break
             time.sleep(2)
-        main.loading()
+        self.loading()
 
-
-    def loading():
-        while not wrappers.onscreen("./captures/1-1.png"):
+    def loading(self):
+        while not Wrappers.onscreen("./captures/1-1.png"):
             time.sleep(1)
         print("Match starting!")
-        main.start()
+        self.start()
 
-
-    def start():
-        while wrappers.onscreen("./captures/1-1.png"):
+    def start(self):
+        while Wrappers.onscreen("./captures/1-1.png"):
             auto.moveTo(888, 376)
-            wrappers.click_right()
+            Wrappers.click_right()
         print("In the match now!")
-        main.main()
+        self.main()
 
-
-    def buy(iterations):
-        wanted_champs = main.get_combo(1)
+    def buy(self, iterations: int):
+        wanted_champs = self.get_combo(1)
         for i in range(iterations):
             for x in wanted_champs:
-                wrappers.click_to("./captures/champions/{}.png".format(x.lower()))
+                Wrappers.click_to("./captures/champions/{}.png".format(x.lower()))
 
-    def get_combo(set:str=1):
+    def get_combo(self, set: str = 1):
         wanted_champs = []
         set = "Set{}".format(int(set))
         for x in list(champs_req["data"]):
@@ -249,53 +324,53 @@ class main:
         # print("Strategy: {}".format(wanted_champs))
         return wanted_champs
 
-    def main():
-        while not wrappers.onscreen("./captures/2-4.png"):
-            main.orbs(1)
-            main.buy(1)
+    def main(self):
+        while not Wrappers.onscreen("./captures/2-4.png"):
+            self.orbs(1)
+            self.buy(1)
             time.sleep(1)
         print("2-4 reached")
-        while wrappers.onscreen("./captures/2-4.png"):
+        while Wrappers.onscreen("./captures/2-4.png"):
             auto.moveTo(928, 396)
-            wrappers.click_right()
+            Wrappers.click_right()
             time.sleep(0.25)
         time.sleep(5)
 
-        if wrappers.onscreen("./captures/2-5.png"):
+        if Wrappers.onscreen("./captures/2-5.png"):
             print("2-5 reached")
-            while not wrappers.onscreen("./captures/3-2.png"): # change this if you want to surrender at a different stage
-                main.orbs(1)
-                main.buy(1)
+            while not Wrappers.onscreen("./captures/3-2.png"): # change this if you want to surrender at a different stage
+                self.orbs(1)
+                self.buy(1)
                 pydirectinput.press('d')
                 time.sleep(1)
-        if wrappers.onscreen("./captures/3-2.png"): # (and this)
+        if Wrappers.onscreen("./captures/3-2.png"): # (and this)
             print("Surrendering now!")
-            main.surrender()
+            self.surrender()
 
-    def orbs(iterations=1):
+    def orbs(self, iterations: int = 1):
         for i in range(iterations):
-            wrappers.click_to_r("./captures/orb_white.png")
-            wrappers.click_to_r("./captures/orb_blue.png")
-            wrappers.click_to_r("./captures/orb_red.png")
+            Wrappers.click_to_r("./captures/orb_white.png")
+            Wrappers.click_to_r("./captures/orb_blue.png")
+            Wrappers.click_to_r("./captures/orb_red.png")
             # wrappers.click_to("./captures/orb_fortune.png")
 
-    def surrender():
-        while not wrappers.onscreen("./captures/surrender 2.png"):
+    def surrender(self):
+        while not Wrappers.onscreen("./captures/surrender 2.png"):
             pydirectinput.press('enter')
             auto.write("/ff")
             pydirectinput.press('enter')
         time.sleep(1)
-        wrappers.click_to("./captures/surrender 2.png")
+        Wrappers.click_to("./captures/surrender 2.png")
         time.sleep(15)
 
-        while wrappers.onscreen("./captures/missions ok.png"):
-            wrappers.click_to("./captures/missions ok.png")
+        while Wrappers.onscreen("./captures/missions ok.png"):
+            Wrappers.click_to("./captures/missions ok.png")
             time.sleep(5)
-        while wrappers.onscreen("./captures/skip waiting for stats.png"):
-            wrappers.click_to("./captures/skip waiting for stats.png")
+        while Wrappers.onscreen("./captures/skip waiting for stats.png"):
+            Wrappers.click_to("./captures/skip waiting for stats.png")
         time.sleep(5)
-        lcu_data = lcu.connect(PATH)
-        lcu.play_again(lcu_data)
+        lcu_data = LCU.connect(PATH)
+        LCU.play_again(lcu_data)
         time.sleep(10)
         print("Queuing up again!")
 
@@ -333,7 +408,9 @@ if __name__ == '__main__':
            f"https://github.com/Detergent13/tft-bot", "nB")
     print("Bot started, queuing up!")
 
+    m = Main()
+
     while True:
-        main.queue()
+        m.queue()
 
 # End auth + main script
